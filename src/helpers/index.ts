09950c66001ab3ce04736df49ce2a037078fd0e4
Alfin Noviaji <alpinnz@gmail.com>
2023-07-30 20:37:33 +0700

    import index ts

---
+++ src/helpers/index.ts
@@ -0,0 +1,5 @@
+export * from "./base_error";
+export * from "./firebase";
+export * from "./generate";
+export * from "./response";
+export * from "./validator";
---
+++ src/models/city.model.ts
@@ -0,0 +1,7 @@
+export type City = {
+    id: string,
+    name: string,
+    status: boolean,
+    created_at?: Date,
+    updated_at?: Date,
+}
